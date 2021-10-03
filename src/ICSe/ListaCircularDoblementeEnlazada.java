dc90aabf68bd1eda782bf2b339844d3c7667ca4a
FranciscoETSD <tadeo00704@gmail.com>
2021-10-03 10:10:06 -0400

    LISTA CIRCULAR DOBLEMENTE ENLAZADA

---
+++ src/ICSe/ListaCircularDoblementeEnlazada.java
@@ -0,0 +1,81 @@
+package ICSe;
+
+public class ListaCircularDoblementeEnlazada {
+    private Nodo inicio;
+    public ListaCircularDoblementeEnlazada() {
+        inicio = null;
+    }
+
+    public boolean estaVacia() {
+        return inicio == null;
+    }
+     public int tamaño() {
+        int contador = 0;
+        if (!estaVacia()){
+            Nodo ultimo = inicio;
+            while (ultimo.getSiguiente() != inicio) {
+                ultimo = ultimo.getSiguiente();
+
+                contador++;
+            }
+            contador++;
+
+        }
+        return contador;
+     }
+
+     public void insertarFin(Object dato) {
+        if (estaVacia()){
+            Nodo nuevo = new Nodo();
+            nuevo.setDato(dato);
+            nuevo.setSiguiente(nuevo);
+
+            inicio = nuevo;
+        }else{
+            Nodo nuevo = new Nodo();
+            nuevo.setDato(dato);
+            nuevo.setSiguiente(inicio);
+
+            Nodo ultimo = inicio;
+            while (ultimo.getSiguiente() != inicio) {
+                ultimo = ultimo.getSiguiente();
+            }
+            ultimo.setSiguiente(nuevo);
+
+
+
+        }
+     }
+
+     public void eliminarFin(){
+        if(!estaVacia()) {
+            Nodo penultimo = inicio;
+            while (penultimo.getSiguiente().getSiguiente() != inicio){
+                penultimo = penultimo.getSiguiente();
+            }
+            penultimo.setSiguiente(inicio);
+        }
+     }
+     public void mostrarCircular(int vueltas){
+        if(!estaVacia()){
+            int contador = 0;
+
+            Nodo temp = inicio;
+            while (temp != null &&  contador < (tamaño() * vueltas)) {
+                System.out.print(temp.getDato() + "   ");
+
+                temp = temp.getSiguiente();
+
+                contador++;
+            }
+
+            System.out.println();
+        }
+     }
+     public void mostrarAtras(int vueltas) {
+         for (int i = tamaño(); i >= 0;i--)
+         {
+             System.out.print(i+ "  ");
+         }
+    }
+}
---
+++ src/ICSe/Main.java
@@ -0,0 +1,21 @@
+package ICSe;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaCircularDoblementeEnlazada listaCircularDoblementeEnlazada= new ListaCircularDoblementeEnlazada();
+        System.out.println(listaCircularDoblementeEnlazada.estaVacia());
+        System.out.println(listaCircularDoblementeEnlazada.tamaño());
+
+        for (int i = 1; i <= 12; i++) {
+            listaCircularDoblementeEnlazada.insertarFin(i);
+        }
+
+        listaCircularDoblementeEnlazada.mostrarCircular(1);
+        listaCircularDoblementeEnlazada.mostrarAtras(1);
+        System.out.println(listaCircularDoblementeEnlazada.tamaño());
+
+
+
+
+    }
+}
